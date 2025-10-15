8c23c880a5c156008de6947b20c9f69605fca6b6
EaloEafit <lcealoo@eafit.edu.co>
2025-10-14 19:31:37 -0500

    UI

--- Assets/Scripts/Buttons.cs
+++ Assets/Scripts/Buttons.cs
@@ -1,12 +1,35 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+//using UnityEngine.UI;
 
 public class Buttons : MonoBehaviour
 {
+    public GameObject CreditsPanel;
     public void PlayGame()
     {
         SceneManager.LoadScene(1);
     }
+
+    public void GoMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+
+    public void CreditsOn()
+    {
+        CreditsPanel.SetActive(true);
+    }
+
+    public void CreditsOff()
+    {
+        CreditsPanel.SetActive(false);
+    }
+
 
 
 }
